73092075947912741623bf7508f81f7f49645317
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2018-08-23 18:51:55 +0200

    Nits

--- provision-u16.sh
+++ provision-u16.sh
@@ -1,2 +1,8 @@
-sudo apt-get update
-sudo apt-get upgrade
+# Update system
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+# Install docker
+curl -fsSL get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo usermod -aG docker ubuntu
